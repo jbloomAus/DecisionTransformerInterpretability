2ae6ce0588c3c9279d2cf2048a25dd1d5e5f44d3
jbloom-md <joseph@massdynamics.com>
2022-12-20 15:35:55 +0000

    get model working

---
+++ src/model.py
@@ -0,0 +1,252 @@
+from typing import Dict, Union
+
+import einops
+import torch
+import torch as t
+import torch.nn as nn
+import torch.nn.functional as F
+from einops import rearrange
+from torchtyping import TensorType as TT
+from transformer_lens import EasyTransformer, EasyTransformerConfig
+from transformer_lens.HookedTransformerConfig import HookedTransformerConfig
+
+
+class StateEncoder(nn.Module):
+    def __init__(self, n_embed):
+        super(StateEncoder, self).__init__()
+        self.n_embed = n_embed
+        # input has shape 56 x 56 x 3
+        # output has shape 1 x 1 x 512
+        self.conv1 = nn.Conv2d(3, 32, 8, stride=4, padding=0) # 56 -> 13
+        self.conv2 = nn.Conv2d(32, 64, 4, stride=2, padding=0) # 13 -> 5
+        self.conv3 = nn.Conv2d(64, 64, 3, stride=1, padding=0) # 5 -> 3
+        self.flatten = nn.Flatten()
+        self.fc = nn.Linear(576, n_embed)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = self.flatten(x)
+        x = self.fc(x)
+        x = F.relu(x)
+        return x
+
+class PosEmbedTokens(nn.Module):
+    def __init__(self, cfg: Union[Dict, HookedTransformerConfig]):
+        super().__init__()
+        if isinstance(cfg, Dict):
+            cfg = HookedTransformerConfig.from_dict(cfg)
+        self.cfg = cfg
+        self.W_pos = nn.Parameter(torch.empty(self.cfg.n_ctx, self.cfg.d_model))
+
+    def forward(
+        self, tokens: TT["batch", "position"], past_kv_pos_offset: int = 0
+    ) -> TT["batch", "position", "d_model"]:
+        """Tokens have shape [batch, pos]
+        Output shape [pos, d_model] - will be broadcast along batch dim"""
+
+        tokens_length = tokens.size(-2)
+        pos_embed = self.W_pos[:tokens_length, :]  # [pos, d_model]
+        broadcast_pos_embed = einops.repeat(
+            pos_embed, "pos d_model -> batch pos d_model", batch=tokens.size(0)
+        )  # [batch, pos, d_model]
+        return broadcast_pos_embed
+
+class DecisionTransformer(torch.nn.Module):
+    def __init__(self, env, max_game_length: int = 1000):
+        '''
+        model = Classifier(cfg)
+        '''
+        super().__init__()
+
+        self.d_model = 64
+        self.block_size = 10
+        vocab_size = env.action_space.n
+        self.max_timestep = max_game_length
+
+        self.ctx_size = 10*self.block_size # we can handle 10 full timesteps
+
+        # Embedding layers
+        self.pos_emb = nn.Parameter(torch.zeros(1, self.ctx_size, self.d_model))
+        # self.pos_emb = nn.Parameter(torch.zeros(1, self.block_size + 1, self.d_model))
+
+        self.time_embedding = nn.Embedding(self.max_timestep+1, self.d_model)
+        self.state_encoder = StateEncoder(self.d_model)
+        self.action_embedding = nn.Sequential(nn.Embedding(env.action_space.n, self.d_model), nn.Tanh())
+        self.reward_embedding = nn.Sequential(nn.Linear(1, self.d_model), nn.Tanh())
+
+        # Initialize weights
+        nn.init.normal_(self.action_embedding[0].weight, mean=0.0, std=0.02)
+
+        # Transformer
+        cfg = EasyTransformerConfig(
+            n_layers=2,
+            d_model=self.d_model,
+            d_head=32,
+            n_heads=2,
+            d_mlp=128,
+            d_vocab= 64,
+            n_ctx= self.ctx_size,
+            act_fn="relu",
+            normalization_type=None,
+            attention_dir="causal",
+            d_vocab_out=env.action_space.n, #
+        )
+
+        assert cfg.attention_dir == "causal", "Attention direction must be causal"
+        assert cfg.normalization_type is None, "Normalization type must be None"
+
+        self.transformer = EasyTransformer(cfg)
+
+        # Because we passing in tokens, turn off embedding and update the position embedding
+        self.transformer.embed = nn.Identity()
+        self.transformer.pos_embed = PosEmbedTokens(cfg)
+
+    # state, action, and return
+    def forward(self, states, actions, targets=None, rtgs=None, timesteps=None):
+        # states: (batch, block_size, 56, 56, 3)
+        # actions: (batch, block_size, 1)
+        # targets: (batch, block_size, 1)
+        # rtgs: (batch, block_size, 1)
+        # timesteps: (batch, 1, 1) # this seems wrong because the time should be different for each element in the each block (incrememnting by 1)
+
+        # embed states and recast back to (batch, block_size, n_embd)
+        state_embeddings = self.get_state_embeddings(states) # batch_size, block_size, n_embd
+        action_embeddings = self.get_action_embeddings(actions) if actions is not None else None # batch_size, block_size, n_embd or None
+        reward_embeddings = self.get_reward_embeddings(rtgs) # batch_size, block_size, n_embd
+        time_embeddings = self.get_time_embeddings(timesteps) # batch_size, block_size, n_embd
+
+        # use state_embeddings, actions, rewards to go and 
+        token_embeddings = self.get_token_embeddings(
+            state_embeddings = state_embeddings, 
+            action_embeddings = action_embeddings,
+            reward_embeddings = reward_embeddings,
+            time_embeddings = time_embeddings
+        )
+
+        # use transformer to model sequence and return embeddings for states, actions, and rewards
+
+        # since we are passing the transformer an embedding, we need to
+        logits = self.transformer(token_embeddings)
+        # select hidden states for actions
+        logits = self.get_logits(logits, actions)
+        # if we are given some desired targets also calculate the loss
+        loss = self.get_loss(logits, targets)
+
+        return logits, loss
+
+    def get_logits(self, logits, actions):
+        if actions is not None:
+            logits = logits[:, 1::3, :] # only keep predictions from state_embeddings (predictions over actions)
+        elif actions is None:
+            logits = logits[:, 1:, :]
+        else:
+            raise NotImplementedError()
+        
+        return logits
+
+    def get_loss(self, logits, targets):
+        if targets is not None:
+            return F.cross_entropy(logits.reshape(-1, logits.size(-1)), targets.reshape(-1))
+        else: 
+            return None
+
+    def check_input_sizes(self, states, actions, targets):
+        assert states.shape[0] == actions.shape[0] == targets.shape[0], "batch sizes must be the same"
+        assert states.shape[1] == actions.shape[1] == targets.shape[1], "block sizes must be the same"
+
+    def get_time_embeddings(self, timesteps):
+        block_size = timesteps.shape[1]
+        timesteps = rearrange(timesteps, 'batch block time-> (batch block) time')
+        time_embeddings = self.time_embedding(timesteps).squeeze(-2)
+        time_embeddings = rearrange(time_embeddings, '(batch block) n_embd -> batch block n_embd', block=block_size)
+        return time_embeddings
+
+    def get_state_embeddings(self, states):
+        # embed states and recast back to (batch, block_size, n_embd)
+        block_size = states.shape[1]
+        states = rearrange(states, 'batch block height width channel -> (batch block) channel height width')
+        state_embeddings = self.state_encoder(states.type(torch.float32).contiguous()) # (batch * block_size, n_embd)
+        state_embeddings = rearrange(state_embeddings, '(batch block) n_embd -> batch block n_embd', block=block_size)
+        return state_embeddings
+
+    def get_action_embeddings(self, actions):
+        # embed actions
+        block_size = actions.shape[1]
+        actions = rearrange(actions, 'batch block action -> (batch block) action')
+        action_embeddings = self.action_embedding(actions).flatten(1) # I don't see why we need this but we do? Maybe because of the sequential?
+        action_embeddings = rearrange(action_embeddings, '(batch block) n_embd -> batch block n_embd', block=block_size)
+        return action_embeddings
+
+    def get_reward_embeddings(self, rtgs):
+        block_size = rtgs.shape[1]
+        rtgs = rearrange(rtgs, 'batch block rtg -> (batch block) rtg')
+        rtg_embeddings = self.reward_embedding(rtgs.type(torch.float32))
+        rtg_embeddings = rearrange(rtg_embeddings, '(batch block) n_embd -> batch block n_embd', block=block_size)
+        return rtg_embeddings
+
+    def get_token_embeddings(self, 
+        state_embeddings, 
+        time_embeddings, 
+        action_embeddings, 
+        reward_embeddings, 
+        targets = None):
+        '''
+        We need to compose the embeddings for:
+            - states
+            - actions
+            - rewards
+            - time
+
+        Handling the cases where:
+        1. we are training:
+            1. we may not have action yet (reward, state)
+            2. we have (action, state, reward)...
+        2. we are evaluating:
+            1. we have a target "a reward" followed by state
+        
+        1.1 and 2.1 are the same, but we need to handle the target as the initial reward.
+        
+        '''
+        batches = state_embeddings.shape[0]
+        block_size = state_embeddings.shape[1]
+
+        reward_embeddings = reward_embeddings + time_embeddings
+        state_embeddings = state_embeddings   + time_embeddings
+        
+        if action_embeddings is not None:
+            action_embeddings = action_embeddings + time_embeddings
+        if targets:
+            targets = targets + time_embeddings
+
+        # estimate the trajectory length:
+        # 1. if we have actions, we have 3 tokens per timestep
+        # 2. if we don't have actions, we have 2 tokens per timestep (and one time step)
+        timesteps = time_embeddings.shape[1] # number of timesteps
+        if action_embeddings is not None:
+            trajectory_length = timesteps*3 
+        else:
+            trajectory_length = 2 # one timestep, no action yet
+
+        # create the token embeddings
+        token_embeddings = torch.zeros(
+            (batches, trajectory_length, self.d_model),  
+            dtype=torch.float32, device=state_embeddings.device) # batches, blocksize, n_embd
+ 
+        if action_embeddings is not None:
+            token_embeddings[:,::3,:] = reward_embeddings
+            token_embeddings[:,1::3,:] = state_embeddings 
+            token_embeddings[:,2::3,:] = action_embeddings
+        else:
+            token_embeddings[:,0,:] = reward_embeddings[:,0,:]
+            token_embeddings[:,1,:] = state_embeddings[:,0,:]
+
+        if targets is not None:
+            token_embeddings[:,0,:] = targets[:,0,:]
+
+        if trajectory_length > self.transformer.cfg.n_ctx:
+            raise ValueError("Trajectory length is greater than the maximum sequence length for this model")
+            # or we could truncate, deal with this later...
+
+        return token_embeddings
---
+++ tests/test_model.py
@@ -0,0 +1,279 @@
+import pytest
+import torch as t
+from einops import rearrange
+import gymnasium as gym
+from minigrid.wrappers import RGBImgPartialObsWrapper, ImgObsWrapper
+from src.model import DecisionTransformer, StateEncoder
+
+
+def test_state_encoder():
+
+    env = gym.make('MiniGrid-Empty-8x8-v0')
+    env = RGBImgPartialObsWrapper(env) # Get pixel observations
+    env = ImgObsWrapper(env) # Get rid of the 'mission' field
+    obs, _ = env.reset() # This now produces an RGB tensor only
+
+    state_encoder = StateEncoder(64)
+    assert state_encoder is not None
+
+    x = t.tensor(obs).unsqueeze(0).to(t.float32)
+    x = rearrange(x, 'b h w c-> b c h w')
+    x = state_encoder(x)
+    assert x.shape == (1, 64)
+
+# first, we want to know that we can initialize the model
+def test_decision_transformer_init():
+
+    env = gym.make('MiniGrid-Empty-8x8-v0')
+    env = RGBImgPartialObsWrapper(env) # Get pixel observations
+    env = ImgObsWrapper(env) # Get rid of the 'mission' field
+    obs, _ = env.reset() # This now produces an RGB tensor only
+
+    decision_transformer = DecisionTransformer(env)
+    assert decision_transformer is not None
+
+    # our model should have the following:
+
+    # state_encoder
+    assert decision_transformer.state_encoder is not None
+
+    # reward_embedding
+    assert decision_transformer.reward_embedding is not None
+
+    # action_embedding
+    assert decision_transformer.action_embedding is not None
+
+    # time_embedding
+    assert decision_transformer.time_embedding is not None
+
+    # a GPT2-like transformer
+    assert decision_transformer.transformer is not None
+    assert type(decision_transformer.transformer).__name__ == 'HookedTransformer'
+
+def test_get_state_embeddings():
+    
+    env = gym.make('MiniGrid-Empty-8x8-v0')
+    env = RGBImgPartialObsWrapper(env) # Get pixel observations
+    env = ImgObsWrapper(env) # Get rid of the 'mission' field
+    obs, _ = env.reset() # This now produces an RGB tensor only
+
+    decision_transformer = DecisionTransformer(env)
+    assert decision_transformer is not None
+
+    # get state embeddings
+    obs = t.tensor(obs).unsqueeze(0).unsqueeze(0) # add block, add batch
+    state_embeddings = decision_transformer.get_state_embeddings(obs)
+    assert state_embeddings.shape == (1, 1, 64)
+
+def test_get_action_embeddings():
+        
+    env = gym.make('MiniGrid-Empty-8x8-v0')
+    env = RGBImgPartialObsWrapper(env) # Get pixel observations
+    env = ImgObsWrapper(env) # Get rid of the 'mission' field
+    obs, _ = env.reset() # This now produces an RGB tensor only
+
+    decision_transformer = DecisionTransformer(env)
+    assert decision_transformer is not None
+
+    # get action embeddings
+    action = t.tensor([0]).unsqueeze(0).unsqueeze(0) # add block, add batch
+    action_embeddings = decision_transformer.get_action_embeddings(action)
+    assert action_embeddings.shape == (1, 1, 64)
+
+def test_get_reward_embeddings():
+
+    env = gym.make('MiniGrid-Empty-8x8-v0')
+    env = RGBImgPartialObsWrapper(env) # Get pixel observations
+    env = ImgObsWrapper(env) # Get rid of the 'mission' field
+    obs, _ = env.reset() # This now produces an RGB tensor only
+
+    decision_transformer = DecisionTransformer(env)
+    assert decision_transformer is not None
+
+    # get reward embeddings
+    rtgs = t.tensor([0]).unsqueeze(0).unsqueeze(0) # add block, add batch
+    reward_embeddings = decision_transformer.get_reward_embeddings(rtgs)
+    assert reward_embeddings.shape == (1, 1, 64)
+
+def test_get_time_embeddings():
+    
+    env = gym.make('MiniGrid-Empty-8x8-v0')
+    env = RGBImgPartialObsWrapper(env) # Get pixel observations
+    env = ImgObsWrapper(env) # Get rid of the 'mission' field
+    obs, _ = env.reset() # This now produces an RGB tensor only
+
+    decision_transformer = DecisionTransformer(env)
+    assert decision_transformer is not None
+
+    # get time embeddings
+    times = t.tensor([0], dtype=t.long).unsqueeze(0).unsqueeze(0) # add block, add batch
+    time_embeddings = decision_transformer.get_time_embeddings(times)
+    assert time_embeddings.shape == (1, 1, 64)
+
+def test_get_token_embeddings_single_batch():
+        
+    env = gym.make('MiniGrid-Empty-8x8-v0')
+    env = RGBImgPartialObsWrapper(env) # Get pixel observations
+    env = ImgObsWrapper(env) # Get rid of the 'mission' field
+    obs, _ = env.reset() # This now produces an RGB tensor only
+
+    decision_transformer = DecisionTransformer(env)
+    assert decision_transformer is not None
+
+    # get token embeddings
+    obs = t.tensor(obs).unsqueeze(0).unsqueeze(0) # add block, add batch
+    action = t.tensor([0]).unsqueeze(0).unsqueeze(0) # add block, add batch
+    rtgs = t.tensor([0]).unsqueeze(0).unsqueeze(0) # add block, add batch
+    times = t.tensor([0], dtype=t.long).unsqueeze(0).unsqueeze(0) # add block, add batch
+
+    state_embedding = decision_transformer.get_state_embeddings(obs)
+    action_embedding = decision_transformer.get_action_embeddings(action)
+    reward_embedding = decision_transformer.get_reward_embeddings(rtgs)
+    time_embedding = decision_transformer.get_time_embeddings(times)
+
+    token_embeddings = decision_transformer.get_token_embeddings(
+        state_embeddings=state_embedding,
+        action_embeddings=action_embedding,
+        reward_embeddings=reward_embedding,
+        time_embeddings=time_embedding
+    )
+
+    assert token_embeddings.shape == (1, 3, 64)
+
+    # now assert that each of the embeddings can be decomposed into state/action/reward + time
+    # we can do this by checking that the sum of the embeddings is equal to the token embeddings
+    t.testing.assert_close(token_embeddings[0][0]- time_embedding, reward_embedding)
+    t.testing.assert_close(token_embeddings[0][1]- time_embedding, state_embedding)
+    t.testing.assert_close(token_embeddings[0][2]- time_embedding, action_embedding)
+
+
+def test_get_token_embeddings_multi_batch():
+        
+    env = gym.make('MiniGrid-Empty-8x8-v0')
+    env = RGBImgPartialObsWrapper(env) # Get pixel observations
+    env = ImgObsWrapper(env) # Get rid of the 'mission' field
+    obs, _ = env.reset() # This now produces an RGB tensor only
+
+    decision_transformer = DecisionTransformer(env)
+
+    # take several actions, store the observations, actions, returns and timesteps
+    all_obs = []
+    all_actions = []
+    all_returns = []
+    all_timesteps = []
+
+    for i in range(10):
+        action = env.action_space.sample()
+        obs, reward, terminated, truncated, info = env.step(action)
+        all_obs.append(obs)
+        all_actions.append(action)
+        all_returns.append(reward)
+        all_timesteps.append(i)
+
+    # convert to tensors.unsqueeze(0)
+    all_obs = t.tensor(all_obs).to(t.float32).unsqueeze(0)
+    all_actions = t.tensor(all_actions).reshape(-1, 1).unsqueeze(0)
+    all_returns = t.randn((10, 1))
+    all_returns_to_go = all_returns.flip(0).cumsum(0).flip(0).reshape(-1, 1).unsqueeze(0)
+    all_timesteps = t.tensor(all_timesteps).reshape(-1, 1).unsqueeze(0)
+    
+    state_embedding = decision_transformer.get_state_embeddings(all_obs)
+    action_embedding = decision_transformer.get_action_embeddings(all_actions)
+    reward_embedding = decision_transformer.get_reward_embeddings(all_returns_to_go)
+    time_embedding = decision_transformer.get_time_embeddings(all_timesteps)
+
+    token_embeddings = decision_transformer.get_token_embeddings(
+        state_embeddings=state_embedding,
+        action_embeddings=action_embedding,
+        reward_embeddings=reward_embedding,
+        time_embeddings=time_embedding
+    )
+
+    assert token_embeddings.shape == (1, 30, 64)
+
+    # now assert that each of the embeddings can be decomposed into state/action/reward + time
+    # we can do this by checking that the sum of the embeddings is equal to the token embeddings
+    t.testing.assert_close(token_embeddings[:,::3,:],  reward_embedding + time_embedding)
+    t.testing.assert_close(token_embeddings[:,1::3,:], state_embedding + time_embedding)
+    t.testing.assert_close(token_embeddings[:,2::3,:], action_embedding + time_embedding)
+
+def test_forward():
+
+    env = gym.make('MiniGrid-Empty-8x8-v0')
+    env = RGBImgPartialObsWrapper(env) # Get pixel observations
+    env = ImgObsWrapper(env) # Get rid of the 'mission' field
+    obs, _ = env.reset() # This now produces an RGB tensor only
+
+
+    # take several actions, store the observations, actions, returns and timesteps
+    all_obs = []
+    all_actions = []
+    all_returns = []
+    all_timesteps = []
+
+
+    for i in range(10):
+        action = env.action_space.sample()
+        obs, reward, terminated, truncated, info = env.step(action)
+        all_obs.append(obs)
+        all_actions.append(action)
+        all_returns.append(reward)
+        all_timesteps.append(i)
+
+    # convert to tensors.unsqueeze(0)
+    all_obs = t.tensor(all_obs).to(t.float32).unsqueeze(0)
+    all_actions = t.tensor(all_actions).reshape(-1, 1).unsqueeze(0)
+    all_returns = t.randn((10, 1))
+    all_returns_to_go = all_returns.flip(0).cumsum(0).flip(0).reshape(-1, 1).unsqueeze(0)
+    all_timesteps = t.tensor(all_timesteps).reshape(-1, 1).unsqueeze(0)
+    
+    decision_transformer = DecisionTransformer(env)
+
+    logits, _ = decision_transformer(
+        states = all_obs,
+        actions = all_actions,
+        rtgs = all_returns_to_go,
+        timesteps = all_timesteps
+    )
+
+    assert logits.shape == (1, 10, env.action_space.n)
+
+
+def test_forward_no_actions():
+
+    env = gym.make('MiniGrid-Empty-8x8-v0')
+    env = RGBImgPartialObsWrapper(env) # Get pixel observations
+    env = ImgObsWrapper(env) # Get rid of the 'mission' field
+    obs, _ = env.reset() # This now produces an RGB tensor only
+
+    # take several actions, store the observations, actions, returns and timesteps
+    all_obs = []
+    all_actions = []
+    all_returns = []
+    all_timesteps = []
+
+    for i in range(10):
+        action = env.action_space.sample()
+        obs, reward, terminated, truncated, info = env.step(action)
+        all_obs.append(obs)
+        all_actions.append(action)
+        all_returns.append(reward)
+        all_timesteps.append(i)
+
+    # convert to tensors.unsqueeze(0)
+    all_obs = t.tensor(all_obs).to(t.float32).unsqueeze(0)
+    all_actions = t.tensor(all_actions).reshape(-1, 1).unsqueeze(0)
+    all_returns = t.randn((10, 1))
+    all_returns_to_go = all_returns.flip(0).cumsum(0).flip(0).reshape(-1, 1).unsqueeze(0)
+    all_timesteps = t.tensor(all_timesteps).reshape(-1, 1).unsqueeze(0)
+    
+    decision_transformer = DecisionTransformer(env)
+
+    logits, _ = decision_transformer(
+        states = all_obs[:, :1, :, :, :],
+        actions = None,
+        rtgs = all_returns_to_go[:, :1],
+        timesteps = all_timesteps[:, :1]
+    )
+
+    assert logits.shape == (1, 1, env.action_space.n)
