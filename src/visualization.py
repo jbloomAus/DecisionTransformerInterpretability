dabff4aa323f81e7ca4a6a1c7148c542883e8ee6
jbloom-md <joseph@massdynamics.com>
2022-12-22 16:31:07 +0000

    provisional plotting code

---
+++ src/visualization.py
@@ -0,0 +1,34 @@
+from minigrid.core.constants import IDX_TO_OBJECT
+import numpy as np
+import torch
+
+def find_agent(observation):
+    height = observation.shape[0]
+    width = observation.shape[1]
+    for i in range(width):
+        for j in range(height):
+            object = IDX_TO_OBJECT[int(observation[i,j][0])]
+            if object == 'agent':
+                return i, j
+
+
+def render_minigrid_observation(env, observation):
+    if isinstance(observation, np.ndarray):
+        observation = observation.copy() # so we don't edit the original object
+    elif isinstance(observation, torch.Tensor):
+        observation = observation.numpy().copy()
+
+    agent_pos = find_agent(observation)
+    agent_dir = observation[agent_pos[0], agent_pos[1]][2]
+
+    observation[agent_pos[0], agent_pos[1]] = [0,0,0]
+
+    grid, _ = env.grid.decode(observation.astype(np.uint8))
+    
+    i = agent_pos[0]
+    j = agent_pos[1]
+    
+    return grid.render(32, (i,j), agent_dir=agent_dir)
+
+def render_minigrid_observations(env, observations):
+    return np.array([render_minigrid_observation(env, observation) for observation in observations])
