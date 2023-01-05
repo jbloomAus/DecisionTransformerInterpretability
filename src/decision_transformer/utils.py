c2b95755afb78d4035e5d5dd23898aa5b51e77c7
jbloom-md <joseph@massdynamics.com>
2023-01-05 17:04:18 +0000

    add types to args so that wandb gets them

--- src/decision_transformer/utils.py
+++ src/decision_transformer/utils.py
@@ -5,23 +5,24 @@ from dataclasses import dataclass
 
 @dataclass
 class DTArgs:
-    exp_name: str = os.path.basename(globals().get("__file__", "DT_implementation").rstrip(".py"))
-    trajectory_path: str = "trajectories/MiniGrid-Dynamic-Obstacles-6x6-v0.pkl"
-    d_model = 32
-    n_heads = 2
-    d_mlp = 64
-    n_layers = 2
+    exp_name: str = "Dev"
+    trajectory_path: str = "trajectories/MiniGrid-DoorKey-8x8-v0.pkl"
+    d_model: int = 64
+    n_heads: int = 2
+    d_mlp: int = 128
+    n_layers: int = 2
     learning_rate: float = 0.00025
     batch_size: int = 128
-    batches: int = 100
+    batches: int = 1000
     max_len: int = 100
-    pct_traj: float = 0.05
-    weight_decay: float = 0.0
+    pct_traj: float = 1.0
+    weight_decay: float = 0.1
     seed: int = 1
     track: bool = True
     wandb_project_name: str = "DecisionTransformerInterpretability"
     wandb_entity: str = None
-    test_frequency: int = 10
+    test_frequency: int = 100
     test_batches: int = 10
-    eval_frequency: int = 10
-    eval_episodes: int = 50 
+    eval_frequency: int = 100
+    eval_episodes: int = 20
+    initial_rtg: float = 1
