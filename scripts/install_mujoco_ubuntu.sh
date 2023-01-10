c56e8b565ed82175c57c07f058d9e3546e393925
jbloomAus <joseph.i.bloom@gmail.com>
2023-01-10 10:08:30 +0000

    install mujoco since docker container wants it

---
+++ scripts/install_mujoco_ubuntu.sh
@@ -0,0 +1,5 @@
+mkdir -p /root/.mujoco 
+wget https://mujoco.org/download/mujoco210-linux-x86_64.tar.gz -O mujoco.tar.gz 
+tar -xf mujoco.tar.gz -C /root/.mujoco 
+rm mujoco.tar.gz
+echo  "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/root/.mujoco/mujoco210/bin" >> /root/.bashrc
