06a2fc4b75737640bbdfb0292a69739695932240
jbloomAus <joseph.i.bloom@gmail.com>
2023-01-10 20:34:41 +0000

    add comments for later

--- scripts/install_mujoco_ubuntu.sh
+++ scripts/install_mujoco_ubuntu.sh
@@ -3,3 +3,5 @@ wget https://mujoco.org/download/mujoco210-linux-x86_64.tar.gz -O mujoco.tar.gz
 tar -xf mujoco.tar.gz -C /root/.mujoco 
 rm mujoco.tar.gz
 echo  "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/root/.mujoco/mujoco210/bin" >> /root/.bashrc
+# apt install gcc 
+# apt-get install libglew-dev patchelf
