0ca95d5b50157ab44c37fbae0f447e56cc4f1105
jbloom-md <joseph@massdynamics.com>
2023-02-01 11:37:41 +1100

    write wrapper for one hot encoding the env

--- src/environments.py
+++ src/environments.py
@@ -1,10 +1,30 @@
 
+import operator
+from functools import reduce
+
 import gymnasium as gym
 import minigrid
+import numpy as np
+from gymnasium import spaces
+from minigrid.wrappers import ObservationWrapper
+from minigrid.core.constants import COLOR_TO_IDX, OBJECT_TO_IDX, STATE_TO_IDX
 
-
-def make_env(env_id: str, seed: int, idx: int, capture_video: bool, run_name: str, render_mode="rgb_array", max_steps=100, fully_observed=False, video_frequency=50):
+def make_env(
+    env_id: str, 
+    seed: int, 
+    idx: int, 
+    capture_video: bool, 
+    run_name: str, 
+    render_mode="rgb_array", 
+    max_steps=100, 
+    fully_observed=False, 
+    flat_one_hot=False,
+    video_frequency=50
+    ):
     """Return a function that returns an environment after setting up boilerplate."""
+
+    # only one of fully observed or flat one hot can be true.
+    assert not (fully_observed and flat_one_hot), "Can't have both fully_observed and flat_one_hot."
 
     def thunk():
 
@@ -30,6 +50,8 @@ def make_env(env_id: str, seed: int, idx: int, capture_video: bool, run_name: st
         if env_id.startswith("MiniGrid"):
             if fully_observed:
                 env = minigrid.wrappers.FullyObsWrapper(env)
+            elif flat_one_hot:
+                env = FlatOneHotObsWrapper(env)
 
         obs = env.reset(seed=seed)
         env.action_space.seed(seed)
@@ -37,3 +59,46 @@ def make_env(env_id: str, seed: int, idx: int, capture_video: bool, run_name: st
         return env
 
     return thunk
+
+
+# I decided to rewrite the flat obs wrapper since it bugs me 
+# that they only one hot encoded the mission string and not the image.
+class FlatOneHotObsWrapper(ObservationWrapper):
+    """
+    Encode observed "image" state (not image, minigrid schema) using a one-hot scheme and flatten.
+    This wrapper is not applicable to BabyAI environments, given that these have their own language component.
+    """
+
+    def __init__(self, env, maxStrLen=96):
+        super().__init__(env)
+
+        self.maxStrLen = maxStrLen
+        self.numCharCodes = 28
+
+        imgSpace = env.observation_space.spaces["image"]
+        imgSize = reduce(operator.mul, imgSpace.shape, 1)
+
+        max_one_hot_dim_idx = max(
+            [max(OBJECT_TO_IDX.values()), 
+            max(STATE_TO_IDX.values()), 
+            max(COLOR_TO_IDX.values())]
+            )
+
+
+        self.observation_space = spaces.Box(
+            low=0,
+            high=1,
+            shape=(imgSize*max_one_hot_dim_idx,),
+            dtype="uint8",
+        )
+
+    def observation(self, obs):
+        image = obs["image"]
+        
+
+        obs = image.flatten()
+        # one hot encode the image and flatten again us vectorization
+        obs = np.eye(len(OBJECT_TO_IDX))[image.flatten()]
+        obs = obs.flatten()
+
+        return obs
---
+++ tests/test_environments.py
@@ -0,0 +1,93 @@
+import pytest 
+
+from src.environments import make_env
+import numpy as np
+
+def test_make_env():
+    env_id = "MiniGrid-Empty-5x5-v0"
+    seed = 0
+    idx = 0
+    capture_video = False
+    run_name = "test"
+    render_mode = "rgb_array"
+    max_steps = 100
+    fully_observed = False
+    video_frequency = 50
+    
+    env_func = make_env(
+        env_id = env_id,
+        seed = seed,
+        idx = idx,
+        capture_video = capture_video,
+        run_name = run_name,
+        render_mode = render_mode,
+        max_steps = max_steps,
+        fully_observed = fully_observed,
+        video_frequency = video_frequency
+    )
+
+    obs, info = env_func().reset()
+
+    assert isinstance(obs, dict)
+    assert obs["image"].shape == (7,7,3)
+    assert env_func is not None
+
+def test_make_env_fully_observed():
+    env_id = "MiniGrid-Empty-5x5-v0"
+    seed = 0
+    idx = 0
+    capture_video = False
+    run_name = "test"
+    render_mode = "rgb_array"
+    max_steps = 100
+    fully_observed = True
+    video_frequency = 50
+
+    env_func = make_env(
+        env_id = env_id,
+        seed = seed,
+        idx = idx,
+        capture_video = capture_video,
+        run_name = run_name,
+        render_mode = render_mode,
+        max_steps = max_steps,
+        fully_observed = fully_observed,
+        video_frequency = video_frequency
+    )
+    obs, info = env_func().reset()
+
+    assert isinstance(obs, dict)
+    assert obs["image"].shape == (5,5,3)
+    assert env_func is not None
+
+def test_make_env_flat_one_hot():
+    env_id = "MiniGrid-Empty-5x5-v0"
+    seed = 0
+    idx = 0
+    capture_video = False
+    run_name = "test"
+    render_mode = "rgb_array"
+    max_steps = 100
+    fully_observed = False
+    flat_one_hot = True
+    video_frequency = 50
+
+    env_func = make_env(
+        env_id = env_id,
+        seed = seed,
+        idx = idx,
+        capture_video = capture_video,
+        run_name = run_name,
+        render_mode = render_mode,
+        max_steps = max_steps,
+        fully_observed = fully_observed,
+        flat_one_hot = flat_one_hot,
+        video_frequency = video_frequency
+    )
+
+    obs, info = env_func().reset()
+
+    assert isinstance(obs, np.ndarray)
+    assert obs.shape == (7*7*3*11,)
+    assert obs.max() == 1
+    assert env_func is not None
