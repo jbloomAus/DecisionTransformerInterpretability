ff8eb137fcacd128704458458fba86758d75a02a
jbloom-md <joseph@massdynamics.com>
2023-02-11 12:33:34 +1100

    use my minigrid branch and add tests

--- tests/test_environments.py
+++ tests/test_environments.py
@@ -1,5 +1,6 @@
 import pytest
 
+import gymnasium as gym
 from src.environments import make_env
 import numpy as np
 
@@ -32,6 +33,75 @@ def test_make_env():
     assert isinstance(obs, dict)
     assert obs["image"].shape == (7, 7, 3)
     assert env_func is not None
+
+
+def test_make_env_mission_space():
+    env_id = "MiniGrid-ObstructedMaze-1Dl-v0"
+    seed = 0
+    idx = 0
+    capture_video = False
+    run_name = "test"
+    render_mode = "rgb_array"
+    max_steps = 100
+    fully_observed = False
+    video_frequency = 50
+
+    env_func = make_env(
+        env_id=env_id,
+        seed=seed,
+        idx=idx,
+        capture_video=capture_video,
+        run_name=run_name,
+        render_mode=render_mode,
+        max_steps=max_steps,
+        fully_observed=fully_observed,
+        video_frequency=video_frequency
+    )
+
+    env = env_func()
+    obs, info = env.reset()
+
+    assert isinstance(obs, dict)
+    assert obs["image"].shape == (7, 7, 3)
+    assert env_func is not None
+
+
+def test_make_env_mission_space_sync_vector():
+    env_id = "MiniGrid-ObstructedMaze-1Dl-v0"
+    seed = 0
+    idx = 0
+    capture_video = False
+    run_name = "test"
+    render_mode = "rgb_array"
+    max_steps = 100
+    fully_observed = False
+    video_frequency = 50
+    num_envs = 10
+
+    env_func = make_env(
+        env_id=env_id,
+        seed=seed,
+        idx=idx,
+        capture_video=capture_video,
+        run_name=run_name,
+        render_mode=render_mode,
+        max_steps=max_steps,
+        fully_observed=fully_observed,
+        video_frequency=video_frequency
+    )
+
+    envs = gym.vector.SyncVectorEnv(
+        [make_env(env_id, seed + i, i, capture_video, run_name,
+                  max_steps=max_steps,
+                  fully_observed=fully_observed,
+                  ) for i in range(num_envs)]
+    )
+
+    # obs, info = env_func().reset()
+
+    # assert isinstance(obs, dict)
+    # assert obs["image"].shape == (7, 7, 3)
+    # assert env_func is not None
 
 
 def test_make_env_fully_observed():
