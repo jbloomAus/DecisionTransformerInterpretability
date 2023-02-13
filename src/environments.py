ac4ad8791431029b8f89fabb12d5895b5d25ffb0
jbloom-md <joseph@massdynamics.com>
2023-02-14 09:41:10 +1100

    add view size arg to make_env

--- src/environments.py
+++ src/environments.py
@@ -1,5 +1,6 @@
 import gymnasium as gym
-from minigrid.wrappers import OneHotPartialObsWrapper, FullyObsWrapper
+from minigrid.wrappers import OneHotPartialObsWrapper, FullyObsWrapper, ObservationWrapper
+from gymnasium import spaces
 
 
 def make_env(
@@ -12,6 +13,7 @@ def make_env(
     max_steps=100,
     fully_observed=False,
     flat_one_hot=False,
+    agent_view_size=7,
     video_frequency=50
 ):
     """Return a function that returns an environment after setting up boilerplate."""
@@ -45,7 +47,9 @@ def make_env(
         if env_id.startswith("MiniGrid"):
             if fully_observed:
                 env = FullyObsWrapper(env)
-            elif flat_one_hot:
+            if agent_view_size != 7:
+                env = ViewSizeWrapper(env, agent_view_size=agent_view_size)
+            if flat_one_hot:
                 env = OneHotPartialObsWrapper(env)
 
         obs = env.reset(seed=seed)
@@ -55,3 +59,52 @@ def make_env(
         return env
 
     return thunk
+
+
+class ViewSizeWrapper(ObservationWrapper):
+    """
+    Wrapper to customize the agent field of view size.
+    This cannot be used with fully observable wrappers.
+
+    Example:
+        >>> import miniworld
+        >>> import gymnasium as gym
+        >>> import matplotlib.pyplot as plt
+        >>> from minigrid.wrappers import ViewSizeWrapper
+        >>> env = gym.make("MiniGrid-LavaCrossingS11N5-v0")
+        >>> obs, _ = env.reset()
+        >>> obs['image'].shape
+        (7, 7, 3)
+        >>> env_obs = ViewSizeWrapper(env, agent_view_size=5)
+        >>> obs, _ = env_obs.reset()
+        >>> obs['image'].shape
+        (5, 5, 3)
+    """
+
+    def __init__(self, env, agent_view_size=7):
+        super().__init__(env)
+
+        assert agent_view_size % 2 == 1
+        assert agent_view_size >= 3
+
+        self.agent_view_size = agent_view_size
+
+        # Compute observation space with specified view size
+        new_image_space = gym.spaces.Box(
+            low=0, high=255, shape=(agent_view_size, agent_view_size, 3), dtype="uint8"
+        )
+
+        # Override the environment's observation spaceexit
+        self.observation_space = spaces.Dict(
+            {**self.observation_space.spaces, "image": new_image_space}
+        )
+
+    def observation(self, obs):
+        env = self.unwrapped
+
+        grid, vis_mask = env.gen_obs_grid(self.agent_view_size)
+
+        # Encode the partially observable view into a numpy array
+        image = grid.encode(vis_mask)
+
+        return {**obs, "image": image}
--- tests/test_environments.py
+++ tests/test_environments.py
@@ -34,6 +34,37 @@ def test_make_env():
     assert env_func is not None
 
 
+def test_make_env_change_view_size():
+    env_id = "MiniGrid-Empty-5x5-v0"
+    seed = 0
+    idx = 0
+    capture_video = False
+    run_name = "test"
+    render_mode = "rgb_array"
+    max_steps = 100
+    fully_observed = False
+    video_frequency = 50
+
+    env_func = make_env(
+        env_id=env_id,
+        seed=seed,
+        idx=idx,
+        capture_video=capture_video,
+        run_name=run_name,
+        render_mode=render_mode,
+        max_steps=max_steps,
+        agent_view_size=5,
+        fully_observed=fully_observed,
+        video_frequency=video_frequency
+    )
+
+    obs, info = env_func().reset()
+
+    assert isinstance(obs, dict)
+    assert obs["image"].shape == (5, 5, 3)
+    assert env_func is not None
+
+
 def test_make_env_fully_observed():
     env_id = "MiniGrid-Empty-5x5-v0"
     seed = 0
@@ -92,5 +123,39 @@ def test_make_env_flat_one_hot():
 
     assert isinstance(obs, dict)
     assert obs["image"].shape == (7, 7, 20,)
+    assert obs["image"].max() == 1
+    assert env_func is not None
+
+
+def test_make_env_flat_one_hot_view_size_change():
+    env_id = "MiniGrid-Empty-5x5-v0"
+    seed = 0
+    idx = 0
+    capture_video = False
+    run_name = "test"
+    render_mode = "rgb_array"
+    max_steps = 100
+    fully_observed = False
+    flat_one_hot = True
+    video_frequency = 50
+
+    env_func = make_env(
+        env_id=env_id,
+        seed=seed,
+        idx=idx,
+        capture_video=capture_video,
+        run_name=run_name,
+        render_mode=render_mode,
+        max_steps=max_steps,
+        fully_observed=fully_observed,
+        flat_one_hot=flat_one_hot,
+        agent_view_size=5,
+        video_frequency=video_frequency
+    )
+
+    obs, info = env_func().reset()
+
+    assert isinstance(obs, dict)
+    assert obs["image"].shape == (5, 5, 20,)
     assert obs["image"].max() == 1
     assert env_func is not None
