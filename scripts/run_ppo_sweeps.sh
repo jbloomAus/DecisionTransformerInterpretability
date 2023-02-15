90a3010e9769dfa4e86131c2b4a7779adb47c837
jbloom-md <joseph@massdynamics.com>
2023-02-15 23:55:40 +1100

    template and warning about bug

--- scripts/run_ppo_sweeps.sh
+++ scripts/run_ppo_sweeps.sh
@@ -3,5 +3,8 @@
 # echo the python path
 which python
 
-EXP_NAME="MiniGrid-SimpleCrossingS9N3-v0" wandb sweep --project $EXP_NAME \
-     /Users/josephbloom/GithubRepositories/DecisionTransformerInterpretability/sweeps/ppo_sweep_template.yml
+# Current bug, you should run the export command before running the script
+echo 'Current bug, you should run the export command before running the script'
+export EXP_NAME="MiniGrid-RedBlueDoors-8x8-v0"
+EXP_NAME="MiniGrid-RedBlueDoors-8x8-v0" wandb sweep --project ${EXP_NAME}-Sweep \
+    /Users/josephbloom/GithubRepositories/DecisionTransformerInterpretability/sweeps/ppo_sweep_template.yml
