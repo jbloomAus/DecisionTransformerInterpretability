7b97bd91b1f47db2b48f4f04d1a8e314a2539489
jbloom-md <joseph@massdynamics.com>
2023-03-03 11:26:44 -0800

    fix dt train test

--- tests/acceptance/test_dt_train.py
+++ tests/acceptance/test_dt_train.py
@@ -1,6 +1,7 @@
 import pytest
 import torch
-from src.decision_transformer.model import DecisionTransformer
+from src.config import TransformerModelConfig, EnvironmentConfig
+from src.models.trajectory_model import DecisionTransformer
 from src.environments.environments import make_env
 from src.decision_transformer.train import evaluate_dt_agent
 from src.decision_transformer.offline_dataset import TrajectoryDataset
@@ -26,17 +27,37 @@ def test_evaluate_dt_agent():
                    run_name="dev", fully_observed=False, max_steps=30)
     env = env()
 
+    # dt = DecisionTransformer(
+    #     env=env,
+    #     d_model=128,
+    #     n_heads=4,
+    #     d_mlp=256,
+    #     n_layers=2,
+    #     state_embedding_type="grid",  # hard-coded for now to minigrid.
+    #     max_timestep=1000,
+    #     n_ctx=3,  # one timestep of context
+    #     device="cuda" if torch.cuda.is_available() else "cpu",
+    # )  # Our DT must have a context window large enough
+
     dt = DecisionTransformer(
-        env=env,
-        d_model=128,
-        n_heads=4,
-        d_mlp=256,
-        n_layers=2,
-        state_embedding_type="grid",  # hard-coded for now to minigrid.
-        max_timestep=1000,
-        n_ctx=3,  # one timestep of context
-        device="cuda" if torch.cuda.is_available() else "cpu",
-    )  # Our DT must have a context window large enough
+        environment_config=EnvironmentConfig(
+            env_id=env_id,
+            one_hot_obs=trajectory_data_set.observation_type == "one_hot",
+            view_size=7,  # trajectory_data_set.metadata['args']['view_size'],
+            fully_observed=False,
+            capture_video=False,
+            render_mode='rgb_array',
+            max_steps=1000),
+        transformer_config=TransformerModelConfig(
+            d_model=128,
+            n_heads=4,
+            d_mlp=256,
+            n_layers=2,
+            state_embedding_type="grid",  # hard-coded for now to minigrid.
+            # max_timestep=1000,
+            n_ctx=3,  # one timestep of context
+            device="cuda" if torch.cuda.is_available() else "cpu",
+        ))
 
     batch = 0
     eval_env_func = make_env(
@@ -44,7 +65,7 @@ def test_evaluate_dt_agent():
         seed=batch,
         idx=0,
         capture_video=True,
-        max_steps=min(dt.max_timestep, 10),
+        max_steps=min(dt.environment_config.max_steps, 10),
         run_name=f"dt_eval_videos_{batch}",
         fully_observed=False,
         flat_one_hot=(trajectory_data_set.observation_type == "one_hot"),
