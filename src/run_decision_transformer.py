29c9fc17d13d8c72c15624269b229d0d5cb6fcb4
jbloom-md <joseph@massdynamics.com>
2023-03-08 11:21:18 +1100

    fix various issues

--- src/run_decision_transformer.py
+++ src/run_decision_transformer.py
@@ -1,6 +1,6 @@
 from .decision_transformer.runner import run_decision_transformer
-from .decision_transformer.utils import DTArgs, parse_args
-from .config import RunConfig, TransformerModelConfig, OfflineTrainConfig, EnvironmentConfig
+from .decision_transformer.utils import parse_args
+from .config import RunConfig, TransformerModelConfig, OfflineTrainConfig
 from .environments.environments import make_env
 
 if __name__ == "__main__":
@@ -13,25 +13,29 @@ if __name__ == "__main__":
         cuda=args.cuda,
         track=args.track,
         wandb_project_name=args.wandb_project_name,
-        wandb_entity=args.wandb_entity
+        wandb_entity=args.wandb_entity,
+        trajectory_path=args.trajectory_path
     )
 
+    TIME_EMBEDDING_TYPE = "linear" if args.linear_time_embedding \
+        else "embedding"
+
     transformer_model_config = TransformerModelConfig(
-        trajectory_path=args.trajectory_path,
-        pct_traj=args.pct_traj,
         d_model=args.d_model,
         n_heads=args.n_heads,
         d_mlp=args.d_mlp,
         n_layers=args.n_layers,
         layer_norm=args.layer_norm,
-        linear_time_embedding=args.linear_time_embedding,
+        time_embedding_type=TIME_EMBEDDING_TYPE,
         n_ctx=args.n_ctx
     )
 
     offline_config = OfflineTrainConfig(
+        trajectory_path=args.trajectory_path,
+        pct_traj=args.pct_traj,
         train_epochs=args.train_epochs,
         test_epochs=args.test_epochs,
-        learning_rate=args.learning_rate,
+        lr=args.learning_rate,
         weight_decay=args.weight_decay,
         batch_size=args.batch_size,
         test_frequency=args.test_frequency,
@@ -44,6 +48,7 @@ if __name__ == "__main__":
 
     run_decision_transformer(
         run_config=run_config,
-        transformer_model_config=transformer_model_config,
-        offline_config=offline_config
+        transformer_config=transformer_model_config,
+        offline_config=offline_config,
+        make_env=make_env,
     )
