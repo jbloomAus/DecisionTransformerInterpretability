a4c45da4c4f43a2363238b7ef2c0484ff7c00a53
jbloom-md <joseph@massdynamics.com>
2023-04-07 11:47:55 +1000

    add fixtures to speed up/clean up tests a bit

--- tests/unit/test_offline_dataset.py
+++ tests/unit/test_offline_dataset.py
@@ -5,116 +5,130 @@ from src.decision_transformer.offline_dataset import TrajectoryDataset, Trajecto
 from torch.utils.data import DataLoader, random_split
 from torch.utils.data.sampler import WeightedRandomSampler
 
-PATH = "tests/fixtures/test_trajectories.pkl"
-PATH_COMPRESSED = "tests/fixtures/test_trajectories.xz"
-
 
 def get_len_i_for_i_in_list(l):
     return [len(i) for i in l]
 
 
-def test_trajectory_reader():
-
+@pytest.fixture
+def trajectory_reader_pkl():
+    PATH = "tests/fixtures/test_trajectories.pkl"
     trajectory_reader = TrajectoryReader(PATH)
-    data = trajectory_reader.read()
+    return trajectory_reader
+
+
+@pytest.fixture
+def trajectory_reader_xz():
+    PATH = "tests/fixtures/test_trajectories.xz"
+    trajectory_reader = TrajectoryReader(PATH)
+    return trajectory_reader
+
+
+@pytest.fixture
+def trajectory_dataset():
+    PATH = "tests/fixtures/test_trajectories.pkl"
+    trajectory_dataset = TrajectoryDataset(
+        PATH, max_len=100, pct_traj=1.0, device="cpu")
+    return trajectory_dataset
+
+
+@pytest.fixture
+def trajectory_dataset_xz():
+    PATH = "tests/fixtures/test_trajectories.xz"
+    trajectory_dataset = TrajectoryDataset(
+        PATH, max_len=100, pct_traj=1.0, device="cpu")
+    return trajectory_dataset
+
+
+def test_trajectory_reader_read(trajectory_reader_pkl):
+    data = trajectory_reader_pkl.read()
     assert data is not None
 
 
-def test_trajectory_reader_xz():
-
-    trajectory_reader = TrajectoryReader(PATH_COMPRESSED)
-    data = trajectory_reader.read()
+def test_trajectory_reader_xz(trajectory_reader_xz):
+    data = trajectory_reader_xz.read()
     assert data is not None
 
 
-def test_trajectory_dataset_init():
+def test_trajectory_dataset_init(trajectory_dataset):
 
-    trajectory_data_set = TrajectoryDataset(PATH, pct_traj=1.0, device="cpu")
+    assert trajectory_dataset.num_trajectories == 54
+    assert trajectory_dataset.num_timesteps == 49920
+    assert trajectory_dataset.actions is not None
+    assert trajectory_dataset.rewards is not None
+    assert trajectory_dataset.dones is not None
+    assert trajectory_dataset.returns is not None
+    assert trajectory_dataset.states is not None
+    assert trajectory_dataset.timesteps is not None
+    assert trajectory_dataset.traj_lens.min() > 0
 
-    assert trajectory_data_set.num_trajectories == 54
-    assert trajectory_data_set.num_timesteps == 49920
-    assert trajectory_data_set.actions is not None
-    assert trajectory_data_set.rewards is not None
-    assert trajectory_data_set.dones is not None
-    assert trajectory_data_set.returns is not None
-    assert trajectory_data_set.states is not None
-    assert trajectory_data_set.timesteps is not None
-    assert trajectory_data_set.traj_lens.min() > 0
-
-    assert len(trajectory_data_set.actions) == len(trajectory_data_set.rewards)
-    assert len(trajectory_data_set.actions) == len(trajectory_data_set.dones)
-    assert len(trajectory_data_set.actions) == len(trajectory_data_set.returns)
-    assert len(trajectory_data_set.actions) == len(trajectory_data_set.states)
+    assert len(trajectory_dataset.actions) == len(trajectory_dataset.rewards)
+    assert len(trajectory_dataset.actions) == len(trajectory_dataset.dones)
+    assert len(trajectory_dataset.actions) == len(trajectory_dataset.returns)
+    assert len(trajectory_dataset.actions) == len(trajectory_dataset.states)
 
     # lengths match
     assert get_len_i_for_i_in_list(
-        trajectory_data_set.actions) == get_len_i_for_i_in_list(trajectory_data_set.states)
+        trajectory_dataset.actions) == get_len_i_for_i_in_list(trajectory_dataset.states)
 
     # max traj length is 1000
-    assert max(get_len_i_for_i_in_list(trajectory_data_set.actions)
-               ) == trajectory_data_set.max_ep_len
-    assert trajectory_data_set.max_ep_len == trajectory_data_set.metadata["args"]["max_steps"]
+    assert max(get_len_i_for_i_in_list(trajectory_dataset.actions)
+               ) == trajectory_dataset.max_ep_len
+    assert trajectory_dataset.max_ep_len == trajectory_dataset.metadata["args"]["max_steps"]
 
 
-def test_trajectory_dataset_init_xz():
+def test_trajectory_dataset_init_xz(trajectory_dataset_xz):
 
-    trajectory_data_set = TrajectoryDataset(
-        PATH_COMPRESSED, pct_traj=1.0, device="cpu")
+    trajectory_dataset = trajectory_dataset_xz
 
-    assert trajectory_data_set.num_trajectories == 238
-    assert trajectory_data_set.num_timesteps == 1920
-    assert trajectory_data_set.actions is not None
-    assert trajectory_data_set.rewards is not None
-    assert trajectory_data_set.dones is not None
-    assert trajectory_data_set.returns is not None
-    assert trajectory_data_set.states is not None
-    assert trajectory_data_set.timesteps is not None
+    assert trajectory_dataset.num_trajectories == 238
+    assert trajectory_dataset.num_timesteps == 1920
+    assert trajectory_dataset.actions is not None
+    assert trajectory_dataset.rewards is not None
+    assert trajectory_dataset.dones is not None
+    assert trajectory_dataset.returns is not None
+    assert trajectory_dataset.states is not None
+    assert trajectory_dataset.timesteps is not None
 
-    assert len(trajectory_data_set.actions) == len(trajectory_data_set.rewards)
-    assert len(trajectory_data_set.actions) == len(trajectory_data_set.dones)
-    assert len(trajectory_data_set.actions) == len(trajectory_data_set.returns)
-    assert len(trajectory_data_set.actions) == len(trajectory_data_set.states)
+    assert len(trajectory_dataset.actions) == len(trajectory_dataset.rewards)
+    assert len(trajectory_dataset.actions) == len(trajectory_dataset.dones)
+    assert len(trajectory_dataset.actions) == len(trajectory_dataset.returns)
+    assert len(trajectory_dataset.actions) == len(trajectory_dataset.states)
 
     # lengths match
     assert get_len_i_for_i_in_list(
-        trajectory_data_set.actions) == get_len_i_for_i_in_list(trajectory_data_set.states)
+        trajectory_dataset.actions) == get_len_i_for_i_in_list(trajectory_dataset.states)
 
 
-def test_trajectory_dataset_get_indices_of_top_p_trajectories_1():
+def test_trajectory_dataset_get_indices_of_top_p_trajectories_1(trajectory_dataset):
 
-    trajectory_data_set = TrajectoryDataset(PATH, pct_traj=1.0, device="cpu")
-    indices = trajectory_data_set.get_indices_of_top_p_trajectories(
-        pct_traj=1.0)
+    indices = trajectory_dataset.get_indices_of_top_p_trajectories(1.0)
 
     # 1. the length of the indices is correct
     assert len(indices) == 54
 
     # 2. The rewards go in ascending order.
     for i in range(len(indices)-1):
-        assert trajectory_data_set.returns[indices[i]
-                                           ] <= trajectory_data_set.returns[indices[i+1]]
+        assert trajectory_dataset.returns[indices[i]
+                                          ] <= trajectory_dataset.returns[indices[i+1]]
 
 
-def test_trajectory_dataset_get_indices_of_top_p_trajectories_01():
+def test_trajectory_dataset_get_indices_of_top_p_trajectories_01(trajectory_dataset):
 
-    trajectory_data_set = TrajectoryDataset(PATH, pct_traj=1.0, device="cpu")
-    indices = trajectory_data_set.get_indices_of_top_p_trajectories(
-        pct_traj=0.1)
+    indices = trajectory_dataset.get_indices_of_top_p_trajectories(0.1)
 
     # 1. the length of the indices is correct
     assert len(indices) == 7
 
     # 2. The rewards go in ascending order.
     for i in range(len(indices)-1):
-        assert trajectory_data_set.returns[indices[i]
-                                           ] <= trajectory_data_set.returns[indices[i+1]]
+        assert trajectory_dataset.returns[indices[i]
+                                          ] <= trajectory_dataset.returns[indices[i+1]]
 
 
-def test_trajectory_dataset__getitem__():
+def test_trajectory_dataset__getitem__(trajectory_dataset):
 
-    trajectory_data_set = TrajectoryDataset(
-        PATH, max_len=100, pct_traj=1.0, device="cpu")
-    s, a, r, d, rtg, timesteps, mask = trajectory_data_set[0]
+    s, a, r, d, rtg, timesteps, mask = trajectory_dataset[0]
 
     assert isinstance(s, torch.Tensor)
     assert isinstance(a, torch.Tensor)
@@ -133,24 +147,22 @@ def test_trajectory_dataset__getitem__():
     assert mask.shape == (100,)
 
 
-def test_trajectory_dataset_sampling_probabilities():
+def test_trajectory_dataset_sampling_probabilities(trajectory_dataset):
 
-    trajectory_data_set = TrajectoryDataset(PATH, pct_traj=1.0, device="cpu")
     assert len(
-        trajectory_data_set.sampling_probabilities) == trajectory_data_set.num_trajectories
-    prob = trajectory_data_set.traj_lens[trajectory_data_set.indices[0]
-                                         ]/trajectory_data_set.num_timesteps
-    assert trajectory_data_set.sampling_probabilities[0] == pytest.approx(
+        trajectory_dataset.sampling_probabilities) == trajectory_dataset.num_trajectories
+    prob = trajectory_dataset.traj_lens[trajectory_dataset.indices[0]
+                                        ]/trajectory_dataset.num_timesteps
+    assert trajectory_dataset.sampling_probabilities[0] == pytest.approx(
         0.02, rel=1e-1)
-    assert trajectory_data_set.sampling_probabilities[0] == prob
-    assert trajectory_data_set.sampling_probabilities[-1] == pytest.approx(
+    assert trajectory_dataset.sampling_probabilities[0] == prob
+    assert trajectory_dataset.sampling_probabilities[-1] == pytest.approx(
         0.0055, rel=1e-1)
-    assert trajectory_data_set.sampling_probabilities.sum() == pytest.approx(1.0, rel=1e-1)
+    assert trajectory_dataset.sampling_probabilities.sum() == pytest.approx(1.0, rel=1e-1)
 
 
-def test_trajectory_dataset_discount_cumusum_10():
+def test_trajectory_dataset_discount_cumusum_10(trajectory_dataset):
 
-    trajectory_data_set = TrajectoryDataset(PATH, pct_traj=1.0, device="cpu")
     vector = torch.tensor([1, 2, 3], dtype=torch.float32)
     discount = 1.0
     expected = torch.tensor([1, 2, 3], dtype=torch.float32)
@@ -158,22 +170,20 @@ def test_trajectory_dataset_discount_cumusum_10():
     expected[1] = expected[1:].sum()
     expected[2] = expected[2:].sum()
 
-    trajectory_data_set = TrajectoryDataset(PATH, pct_traj=1.0, device="cpu")
-    actual = trajectory_data_set.discount_cumsum(vector, discount)
+    actual = trajectory_dataset.discount_cumsum(vector, discount)
     assert actual.shape == expected.shape
     torch.testing.assert_allclose(torch.tensor(actual), expected)
 
 
-def test_trajectory_dataset_as_dataloader():
+def test_trajectory_dataset_as_dataloader(trajectory_dataset):
 
-    dataset = TrajectoryDataset(PATH, max_len=100, pct_traj=1.0, device="cpu")
     sampler = WeightedRandomSampler(
-        weights=dataset.sampling_probabilities,
-        num_samples=dataset.num_trajectories,
+        weights=trajectory_dataset.sampling_probabilities,
+        num_samples=trajectory_dataset.num_trajectories,
         replacement=True,
     )
     dataloader = torch.utils.data.DataLoader(
-        dataset, batch_size=8, sampler=sampler)
+        trajectory_dataset, batch_size=8, sampler=sampler)
 
     for i, (s, a, r, d, rtg, timesteps, mask) in enumerate(dataloader):
         assert s.shape == (8, 100, 7, 7, 3), f"i={i}, s.shape={s.shape}"
@@ -203,15 +213,16 @@ def test_trajectory_dataset_as_dataloader():
             break
 
 
-def test_train_test_split():
-
-    dataset = TrajectoryDataset(PATH, max_len=100, pct_traj=1.0, device="cpu")
+def test_train_test_split(trajectory_dataset):
 
     # Split the dataset into training and testing sets
-    train_dataset, test_dataset = random_split(dataset, [0.80, 0.20])
+    train_dataset, test_dataset = random_split(
+        trajectory_dataset, [0.80, 0.20])
 
-    assert len(train_dataset) == pytest.approx(0.80 * len(dataset), abs=1)
-    assert len(test_dataset) == pytest.approx(0.20 * len(dataset), abs=1)
+    assert len(train_dataset) == pytest.approx(
+        0.80 * len(trajectory_dataset), abs=1)
+    assert len(test_dataset) == pytest.approx(
+        0.20 * len(trajectory_dataset), abs=1)
 
     s, a, r, d, rtg, timesteps, mask = train_dataset[0]
     assert s.shape == (100, 7, 7, 3)
@@ -233,7 +244,7 @@ def test_train_test_split():
 
     # Create the train DataLoader
     train_sampler = torch.utils.data.sampler.WeightedRandomSampler(
-        weights=dataset.sampling_probabilities[train_dataset.indices],
+        weights=trajectory_dataset.sampling_probabilities[train_dataset.indices],
         num_samples=len(train_dataset),
         replacement=True,
     )
@@ -242,7 +253,7 @@ def test_train_test_split():
 
     # Create the test DataLoader
     test_sampler = torch.utils.data.sampler.WeightedRandomSampler(
-        weights=dataset.sampling_probabilities[test_dataset.indices],
+        weights=trajectory_dataset.sampling_probabilities[test_dataset.indices],
         num_samples=len(test_dataset),
         replacement=True,
     )
@@ -250,16 +261,16 @@ def test_train_test_split():
         test_dataset, batch_size=8, sampler=test_sampler)
 
 
-def test_train_test_split_other_data():
-
-    dataset = TrajectoryDataset(
-        PATH_COMPRESSED, max_len=100, pct_traj=1.0, device="cpu")
+def test_train_test_split_other_data(trajectory_dataset_xz):
 
     # Split the dataset into training and testing sets
-    train_dataset, test_dataset = random_split(dataset, [0.80, 0.20])
+    train_dataset, test_dataset = random_split(
+        trajectory_dataset_xz, [0.80, 0.20])
 
-    assert len(train_dataset) == pytest.approx(0.80 * len(dataset), abs=1)
-    assert len(test_dataset) == pytest.approx(0.20 * len(dataset), abs=1)
+    assert len(train_dataset) == pytest.approx(
+        0.80 * len(trajectory_dataset_xz), abs=1)
+    assert len(test_dataset) == pytest.approx(
+        0.20 * len(trajectory_dataset_xz), abs=1)
 
     s, a, r, d, rtg, timesteps, mask = train_dataset[0]
     assert s.shape == (100, 7, 7, 20)
@@ -281,7 +292,7 @@ def test_train_test_split_other_data():
 
     # Create the train DataLoader
     train_sampler = torch.utils.data.sampler.WeightedRandomSampler(
-        weights=dataset.sampling_probabilities[train_dataset.indices],
+        weights=trajectory_dataset_xz.sampling_probabilities[train_dataset.indices],
         num_samples=len(train_dataset),
         replacement=True,
     )
@@ -290,7 +301,7 @@ def test_train_test_split_other_data():
 
     # Create the test DataLoader
     test_sampler = torch.utils.data.sampler.WeightedRandomSampler(
-        weights=dataset.sampling_probabilities[test_dataset.indices],
+        weights=trajectory_dataset_xz.sampling_probabilities[test_dataset.indices],
         num_samples=len(test_dataset),
         replacement=True,
     )
