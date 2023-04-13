e9d6c8124a5ac1fd0b66e3a3344a98812a42a195
jbloom-md <joseph@massdynamics.com>
2023-04-14 09:53:49 +1000

    make it easy to debug memory env run

--- tests/end_end/test_decision_transformer.py
+++ tests/end_end/test_decision_transformer.py
@@ -121,3 +121,52 @@ def test_clone_transformer(download_training_data, n_ctx):
     )
 
     print("Test passed! Look at wandb and compare to the previous run.")
+
+
+# use this test when wanting to debug a specific script
+def test_decision_transformer_bespoke():
+    run_config = RunConfig(
+        exp_name="MiniGrid-MemoryS7FixedStart-v0",
+        wandb_project_name="DecisionTransformerInterpretability",
+        seed=1,
+        track=False,
+    )
+
+    transformer_model_config = TransformerModelConfig(
+        d_model=128,
+        n_heads=4,
+        d_mlp=256,
+        n_layers=2,
+        n_ctx=38,
+        time_embedding_type="embedding",
+        seed=1,
+        device="cpu",
+    )
+
+    offline_config = OfflineTrainConfig(
+        trajectory_path="trajectories/MiniGrid-MemoryS7FixedStart-v0-Checkpoint11.gz",
+        batch_size=32,
+        lr=0.0001,
+        weight_decay=0.01,
+        pct_traj=1,
+        prob_go_from_end=0.5,
+        device="cpu",
+        track=run_config.track,
+        train_epochs=30,
+        test_epochs=1,
+        test_frequency=3,
+        eval_frequency=3,
+        eval_episodes=10,
+        initial_rtg=[0, 1],
+        eval_max_time_steps=50,
+        model_type="decision_transformer",
+        eval_num_envs=16,
+        convert_to_one_hot=True,
+    )
+
+    run_decision_transformer(
+        run_config=run_config,
+        transformer_config=transformer_model_config,
+        offline_config=offline_config,
+        make_env=make_env,
+    )
