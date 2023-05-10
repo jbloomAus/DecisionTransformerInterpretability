3438b07fb573a38df24925efb2082767b8101757
jbloom-md <joseph@massdynamics.com>
2023-05-10 16:00:23 +1000

    add package that streamlit now needs apparently

--- src/streamlit_app/model_index.py
+++ src/streamlit_app/model_index.py
@@ -7,5 +7,5 @@ model_index = {
     # The original post, reproduced with 1/10th of the total training epochs
     "models/MiniGrid-Dynamic-Obstacles-8x8-v0/ReproduceOriginalPostShort.pt": "ReproduceOriginalPostShort",
     "models/MiniGrid-MemoryS7FixedStart-v0/DevModelNotCalibrated.pt": "MemoryDevNotCalibrated",
-    "dev/artifacts/MiniGrid-MemoryS7FixedStart-v0__MiniGrid-MemoryS7FixedStart-v0__1__1681336425:v0/MiniGrid-MemoryS7FixedStart-v0__MiniGrid-MemoryS7FixedStart-v0__1__1681336425.pt": "MemoryDevNotCalibrated2",
+    "models/MiniGrid-MemoryS7FixedStart-v0/WorkingModel.pt": "MemoryWorkingModel",
 }
